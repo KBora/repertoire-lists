f4fbf8531f733e4339c0fb9d4cd7f9ed5b50b99a
wookkii <wookkii@home>
2020-05-04 23:30:41 +1000

    Install express and add routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-import 'dotenv/config';
+// import 'dotenv/config';
  
-console.log('Hello Node.js project.');
+// console.log('Hello Node.js project.');
  
-console.log(process.env.MY_SECRET);
+// console.log(process.env.MY_SECRET);
+
+import express from 'express';
+ 
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+app.get('/example', (req, res) => {
+    res.send('Hello example!');
+  });
+
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+);
