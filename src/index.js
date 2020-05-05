50f5d29c62e5affae7274ab1a4cfb5bbafe0a2d6
wookkii <wookkii@home>
2020-05-06 08:26:34 +1000

    Generate uuid . Use express middleware.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 // console.log('Hello Node.js project.');
- 
-
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
- 
+import { v4 as uuidv4 } from 'uuid';
+
 let users = {
     1: {
       id: '1',
@@ -36,6 +35,8 @@ let users = {
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/users', (req, res) => {
     return res.send(Object.values(users));
@@ -52,6 +53,21 @@ app.get('/messages', (req, res) => {
 app.get('/messages/:messageId', (req, res) => {
     return res.send(messages[req.params.messageId]);
 })
+
+app.post('/messages', (req, res) => {
+    const id = uuidv4();
+    const message = {
+      id,
+      text: req.body.text,
+    };
+   
+    // const date = Date.parse(req.body.date);
+    // const count = Number(req.body.count);
+    
+    messages[id] = message;
+   
+    return res.send(message);
+  });
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
